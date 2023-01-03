4c69d000418d365dd07f2ba471b5f8c6f8b124c4
ridhailham <117886100+ridhailham@users.noreply.github.com>
2023-01-03 14:52:49 +0700

    Add files via upload

---
+++ Input-Data-Ke-Dalam-Array.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	arg := os.Args[1:]
+
+	var (
+		hasil float64
+		angka [5]float64
+		total float64
+	)
+
+	if l := len(arg); l == 0 || l > 5 {
+		fmt.Println("Wajib masukkan angka minimal 1 dan maksimal 5")
+		return
+	}
+
+	for i, v := range arg {
+		n, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			continue
+		}
+
+		angka[i] = n
+
+		hasil += n
+		total++
+	}
+
+	integer := len(angka)
+
+	fmt.Printf("Angka Anda: %d\n", integer)
+	fmt.Printf("Rata - rata: %.1f\n", hasil/total)
+}
